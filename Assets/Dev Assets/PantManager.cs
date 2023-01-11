a92d273b7e2ee72cddaf93b8f454b9f78359d5fd
Dev_ <patrykos1010@gmail.com>
2023-01-11 12:05:49 +0100

    DevAsset Addition
    Explanatory

---
+++ Assets/Dev Assets/PantManager.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PantManager : MonoBehaviour
+{
+
+    public int pantpoint;
+    public int pantbttl;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Kommer att skriva in om pant, UI och vart det ska användas senare. TBD
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // TBD
+    }
+}
