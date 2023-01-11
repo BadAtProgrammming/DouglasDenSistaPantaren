6c922995a6196bbef7e922ba5e490745551a7897
BadAtProgrammming <benjamin.nord@elev.ga.lbs.se>
2023-01-11 14:27:04 +0100

    ey
    Flyttade grejer till mitt egen folder

--- Assets/Benjamins Stuff/ShopSystem.cs
+++ Assets/Benjamins Stuff/ShopSystem.cs
@@ -2,17 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PickUp : MonoBehaviour
+public class ShopSystem : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
-       bool PickUpWeapon = true;  
+       
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+       
     }
 }
