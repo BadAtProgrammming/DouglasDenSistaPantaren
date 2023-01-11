a3ac733d9ec281884101078a7b103f06e66e06f0
BadAtProgrammming <benjamin.nord@elev.ga.lbs.se>
2023-01-11 12:31:09 +0100

    e
    e

--- Assets/Attacks.cs
+++ Assets/Attacks.cs
@@ -4,6 +4,13 @@ using UnityEngine;
 
 public class Attacks : MonoBehaviour
 {
+
+
+
+
+    
+    
+    
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +20,23 @@ public class Attacks : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        float PunchTimer = Time.deltaTime;
+        float KickTimer = Time.deltaTime;
+
+        bool ReadyKick = true;
+        bool ReadyPunch = true;
+
+
+
+        if (Input.GetKey(KeyCode.R) && ReadyKick == true)
+        {
+            print("Kick");
+            KickTimer = 0;
+        }
+        if (Input.GetKey(KeyCode.E) && ReadyPunch == true) 
+        {
+            print("Punch");
+            PunchTimer = 0;
+        }
     }
 }
