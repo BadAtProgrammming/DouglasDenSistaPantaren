5bc4c53eaa654223eb7154b45d3c0e396f97efa2
CaffeineAdictedAlpaca <119925733+CaffeineAdictedAlpaca@users.noreply.github.com>
2023-01-13 12:16:02 +0100

    Score.text

--- Assets/Sixten Assets/PantScore.cs
+++ Assets/Sixten Assets/PantScore.cs
@@ -5,18 +5,20 @@ using TMPro;
 
 public class PantScore : MonoBehaviour
 {
+    TextMeshProUGUI ScoreText;
+
     [SerializeField]
     public int score = 0;
     // Start is called before the first frame update
     void Start()
     {
-        
+        ScoreText = FindObjectOfType<TextMeshProUGUI>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        ScoreText.text = "Pant: " + score;
     }
     private void OnCollisionEnter2D(Collision2D collision)
     {
