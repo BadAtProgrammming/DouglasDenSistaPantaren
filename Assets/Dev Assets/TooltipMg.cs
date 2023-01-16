af70db57d8088f9aaf0344fa0acfdad18efd470c
Dev_ <patrykos1010@gmail.com>
2023-01-16 12:23:45 +0100

    Deathscene code omgomgomg
    AAAAAAAAA

--- Assets/Dev Assets/TooltipMg.cs
+++ Assets/Dev Assets/TooltipMg.cs
@@ -6,15 +6,55 @@ using TMPro;
 
 public class TooltipMg : MonoBehaviour
 {
+    string[] Names = { };
+    string[] typeitems = { };
+    string[] costs = { };
+    string[] descs = { };
+    string[] statistics = { };
+
     [SerializeField] private CanvasGroup canvasGroup;
 
-    // Start is called before the first frame update
+    //Make this a speech bubble cuz i didnt make it an array -Dev
+    private Image itemimage;
+    private GameObject ImageOnCanvas;
+
+    private TextMeshProUGUI Name;
+    private GameObject NameCanvas;
+
+    private TextMeshProUGUI ItemType;
+    private GameObject ItemTypeCanvas;
+
+    private TextMeshProUGUI Cost;
+    private GameObject CostCanvas;
+
+    private TextMeshProUGUI Desc;
+    private GameObject DescCanvas;
+
+    private TextMeshProUGUI Stats;
+    private GameObject StatCanv;
+
     void Start()
     {
-        
+        //I hate defining shit on fucking god -Dev
+        ImageOnCanvas = GameObject.Find("ItemImg");
+        itemimage = ImageOnCanvas.GetComponent<Image>();
+
+        NameCanvas = GameObject.Find("Name");
+        Name = NameCanvas.GetComponent<TextMeshProUGUI>();
+
+        ItemTypeCanvas = GameObject.Find("Itemtype");
+        ItemType = ItemTypeCanvas.GetComponent<TextMeshProUGUI>();
+
+        CostCanvas = GameObject.Find("Cost");
+        Cost = CostCanvas.GetComponent<TextMeshProUGUI>();
+
+        DescCanvas = GameObject.Find("Desc");
+        Desc = DescCanvas.GetComponent<TextMeshProUGUI>();
+
+        StatCanv = GameObject.Find("Statistics");
+        Stats = StatCanv.GetComponent<TextMeshProUGUI>();
     }
 
-    // Update is called once per frame
     void Update()
     {
         
