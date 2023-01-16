c3076753112f99e8a3366727477398acd0f14603
Dev_ <patrykos1010@gmail.com>
2023-01-16 15:25:16 +0100

    Tooltip Addition
    HOO AH

--- Assets/Dev Assets/TooltipMg.cs
+++ Assets/Dev Assets/TooltipMg.cs
@@ -6,11 +6,11 @@ using TMPro;
 
 public class TooltipMg : MonoBehaviour
 {
-    string[] Names = { };
-    string[] typeitems = { };
-    string[] costs = { };
-    string[] descs = { };
-    string[] statistics = { };
+    string[] Names = {"Sign", "Axe", "Frying Pan", "LightSaber", "Microsoft Edge", "Umbrella" };
+    string[] typeitems = {"Blunt", "Axe Body Spray", "Scout TF2", "luminescent plasma blade", "Blade", "Umbrella" };
+    string[] costs = {"Free Bruh", "46", "60", "100", "64", "30" };
+    string[] descs = { "This is a sign from god, that you should just shut the fuck up. No but really shut up!  Anyways this weapon is a basic weapon, stop crying that its underpowered, you are basically hitting people with a wooden board.", "BONK BONK BONK -Scout TF2 This item is also very effective if you are fighting sans and need extra health. No sans is not in this game, play undertale if you want sans.", "CTRL C + CTRL V description from wikipedia. Source: I made it the fuck up.  What this is though is a laser blade thing that cuts people, thats it.", "This item is very special, as it is the only item with a !!SPECIAL!! ability that finishes the game immediately, it is activated by pressing ALT + F4. You should try it!", "This item shields you from water, but also from enemies apparently, yeah i know its an umbrella, its very effective though." };
+    string[] statistics = {"10 Dmg", "20-50", "15 Dmg", "30 Dmg", "25 Dmg", "15 Dmg"};
 
     [SerializeField] private CanvasGroup canvasGroup;
 
