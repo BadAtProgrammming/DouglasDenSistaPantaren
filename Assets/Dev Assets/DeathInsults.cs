a2f256a3702a5a23ae32f70a708d82c6f3047b22
Dev_ <patrykos1010@gmail.com>
2023-01-17 12:28:13 +0100

    Pixel Font

--- Assets/Dev Assets/DeathInsults.cs
+++ Assets/Dev Assets/DeathInsults.cs
@@ -2,20 +2,21 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using UnityEngine.UI;
 
 public class DeathInsults : MonoBehaviour
 {
     //Ok i made an array -Dev
     string[] insults = { "Have you considered to block, parry and dodge?", "Skill issue", "Bro you suck", "Bad ending", "L", "You sweat games and you still lose?", "Uninstall", "My guy go back to atari", "Your finger slipped right?", "According to my statistics, you suck!", "RDMRDMRDMRDMRDMRDMRDMRDM", "Cope", "Stage 1: Denial", "1v1 me rn bro!!!!!1!", "You should stop playing Dating Sims" };
 
-    TextMeshProUGUI splashText;
+    Text splashText;
     float timer = 10f;
 
     // Start is called before the first frame update
     void Start()
     {
         //Define and shit -Dev
-        splashText = GetComponent<TextMeshProUGUI>();
+        splashText = GetComponent<Text>();
     }
 
     // Update is called once per frame
