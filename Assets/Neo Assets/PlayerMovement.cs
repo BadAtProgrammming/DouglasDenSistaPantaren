d472a2e60f4426d8fe4498d595851d321d666829
CaffeineAdictedAlpaca <119925733+CaffeineAdictedAlpaca@users.noreply.github.com>
2023-01-18 09:55:42 +0100

    player size

--- Assets/Neo Assets/PlayerMovement.cs
+++ Assets/Neo Assets/PlayerMovement.cs
@@ -37,14 +37,14 @@ public class PlayerMovement :MonoBehaviour
         if (Input.GetKey("d") && canRight == true) //movement
         {
             transform.position = transform.position += transform.right * playerSpeed * Time.deltaTime;
-            transform.localScale = new Vector2(4,4 );
+            transform.localScale = new Vector2(3.5f,3.5f );
             
         }
        
         if (Input.GetKey("a") && canLeft == true)
         {
             transform.position = transform.position -= transform.right * playerSpeed * Time.deltaTime;
-            transform.localScale = new Vector2(-4,4 );
+            transform.localScale = new Vector2(-3.5f,3.5f );
         }
         
         if (Input.GetKey("w") && canUp == true)
