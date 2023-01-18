860f88c7ac560926d8c54f39bda7bff2af3445a9
NEOBENGUIN <daniel.wan@elev.ga.lbs.se>
2023-01-18 10:38:15 +0100

    made enemy drop pant :)

--- Assets/Sixten Assets/GoonScript.cs
+++ Assets/Sixten Assets/GoonScript.cs
@@ -4,32 +4,23 @@ using UnityEngine;
 
 public class GoonScript : MonoBehaviour
 {
-    [SerializeField]
-    GameObject thingToSpawn;
+    [SerializeField] GameObject thingToSpawn;
 
+    private bool candie = true;
     private Transform EnemyPosition;
     public bool inReach;
+
     [SerializeField] int Health = 20;
+
     // Start is called before the first frame update
     void Start()
     {
         EnemyPosition = GetComponent<Transform>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Die()
     {
-        if (Input.GetKeyDown(KeyCode.N) && inReach == true)
-        {
-            
-            Health -= 10;
-        }
-        if(Health<= 0)
-        {
-            Instantiate(thingToSpawn, EnemyPosition);
-            
-            Destroy(gameObject);
-        }
+        Destroy(gameObject);
     }
     private void OnTriggerEnter2D(Collider2D collision)
     {
@@ -40,7 +31,23 @@ public class GoonScript : MonoBehaviour
         else
         {
             inReach = false;
-            
+
+        }
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.N) && inReach == true)
+        {
+
+            Health -= 10;
+        }
+
+        if (Health <= 0 && candie)
+        {
+            candie = false;
+            Instantiate(thingToSpawn, EnemyPosition.position, Quaternion.identity);
+            Invoke("Die", 1);
         }
     }
 }
