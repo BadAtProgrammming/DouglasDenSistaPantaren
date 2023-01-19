9959665c2e81d0e0f7118d0cf6f16e809c70267c
Dev_ <patrykos1010@gmail.com>
2023-01-19 17:57:40 +0100

    Deathcanvas fix (Jesus fucking christ)
    I wanna KMS LMAO

--- Assets/Sixten Assets/PlayerHelth.cs
+++ Assets/Sixten Assets/PlayerHelth.cs
@@ -7,40 +7,31 @@ using TMPro;
 public class PlayerHelth : MonoBehaviour
 {
     
-
-    GameObject CanvasObject;
-
-    
+    [SerializeField]
+    RectTransform transformobject;
     public int Health;
-    
+
+    bool activated = false;
 
     void Start()
     {
-        
-        //Sets object reference to find the deathcanvas, can also be set to a tag -Dev
-        CanvasObject = GameObject.Find("DeathCanvas");
+        //References spew out errors nonetheless... I cant fix it for some reason, this is just the best solution -Dev
     }
 
     void Update()
     {
-       
 
         //If you have no heath: show death screen -Sixten
-        if (Health <= 0)
+        if (Health <= 0 && activated == false)
         {
-            
+            //Avoids errors by activating it only once, this makes constantly spewing errors & lagging the game impossible -Dev
             GameObject.Find("player").GetComponent<PlayerMovement>().enabled = false; //disables player movement when health <= 0
             GameObject.Find("Main Camera").GetComponent<CameraMovement>().enabled = false;//disables camera movement when health <= 0
             GameObject.Find("pHurtbox").GetComponent<PlayerTakeDamage>().enabled = false;
             //When health 0 activates the entire canvasobject and its components -Dev
-            CanvasObject.SetActive(true);
+            transformobject.anchoredPosition = new Vector3(640f, 360f, 0f);
+            activated = true;
         }
-        else if (Health > 0)
-        {
-            //When Health is above 0 it deactivates it... Duuh -Dev
-            CanvasObject.SetActive(false);
-        }
-        
     }
     public void TakeDamage(int amount)
     {
