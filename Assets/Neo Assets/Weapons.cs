a39b2b528930180081db3e03c4aa4d93b6a3726b
CaffeineAdictedAlpaca <119925733+CaffeineAdictedAlpaca@users.noreply.github.com>
2023-01-20 11:35:22 +0100

    Nu funkar shop

--- Assets/Neo Assets/Weapons.cs
+++ Assets/Neo Assets/Weapons.cs
@@ -16,7 +16,6 @@ public class Weapons : MonoBehaviour
     PantScore Pant;
     
     public Animator anim;
-    Animator anim;
     public bool Unarmed;
     public bool HasWeapon;
     public bool Sign;
