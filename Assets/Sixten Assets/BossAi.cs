d3a612bf0f1e7fba369efe2a5c9689562ba8b90b
CaffeineAdictedAlpaca <119925733+CaffeineAdictedAlpaca@users.noreply.github.com>
2023-01-24 10:26:33 +0100

    Boss inte funkar

---
+++ Assets/Sixten Assets/BossAi.cs
@@ -0,0 +1,154 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+
+public class BossAi : MonoBehaviour
+{
+
+    float Speed = 2;
+    Rigidbody2D rb;
+    private enum State
+    {
+        Idle,
+        Chase,
+        Attack,
+        IdleMoving //States to group together actions
+    }
+
+    private State _currentState; // represents current state of AI
+
+    public Transform player; // So the asset can use transform everywhere in the code.
+    Coroutine coroutine;
+    private SpriteRenderer spriteRenderer;
+
+
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>(); // so the asset can reference Rigidbody without having to write it in several times.
+        spriteRenderer = GetComponent<SpriteRenderer>();
+
+    }
+
+    private void Update()
+    {
+
+        float DisengageRange = 21;
+        float Reach = 2;
+        float detectRange = 20;
+        detectRange *= detectRange;
+        float playerdistance = (player.position - transform.position).sqrMagnitude; // responsible for if the ai can see the player or not
+
+
+        if (_currentState == State.Idle) //Idle doesnt do much, just a state for the other states to change from. Also a State for LookForPlayer() to change to other states
+        {
+            LookForPlayer();
+            print("idle");
+        }
+        if (_currentState == State.Chase) // State for ChasePlayer()
+        {
+            ChasePlayer();
+            print("chasing");
+        }
+        if (_currentState == State.Attack) // State for AttackPlayer()
+        {
+
+            AttackPlayer();
+            print("attack");
+        }
+        if (_currentState == State.IdleMoving) // State for Strafing()
+        {
+            Strafing();
+            print("strafe");
+        }
+        void LookForPlayer()
+        {
+
+
+
+            if (playerdistance <= DisengageRange)  // Checks if player is close enough to enemy
+            {
+                _currentState = State.Chase;
+            }
+
+
+
+        }
+
+        void ChasePlayer() // Code for the enemy to chase the player
+        {
+            spriteRenderer.flipX = player.transform.position.x > transform.position.x;
+            if (playerdistance <= detectRange)
+            {
+                rb.velocity = Vector2.zero;
+                Vector2 velocity = (player.transform.position - transform.position).normalized * Speed;
+                rb.velocity = velocity;
+                transform.localScale = new Vector2(-10, 10);
+
+
+            }
+            if (playerdistance <= Reach)
+            {
+                rb.velocity = Vector2.zero;
+                print("attack");
+                _currentState = State.Attack;
+                transform.localScale = new Vector2(10, 10);
+            }
+            else if (playerdistance >= DisengageRange)
+            {
+                rb.velocity = Vector2.zero;
+                _currentState = State.Idle;
+            }
+
+        }
+        void AttackPlayer() //Code to make the enemy to stop at the player's location and use attack animations to hit them, currently doesnt do much.
+        {
+            spriteRenderer.flipX = player.transform.position.x < transform.position.x; // flips position so the animations are pointed the right way
+            Animator anim;
+            anim = GetComponent<Animator>();
+            if (playerdistance <= Reach)
+            {
+                rb.velocity = Vector2.zero;
+                anim.SetBool("AITTACK", true);
+            }
+            else if (playerdistance >= Reach)
+            {
+                rb.velocity = Vector2.zero;
+                _currentState = State.Chase;
+                anim.SetBool("AITTACK", false);
+            }
+
+
+        }
+        void Strafing()
+        {
+
+            //Code to make enemy character run away after for example player is hit or downed, works?
+            print("wtf");
+            if (_currentState == State.IdleMoving)
+            {
+                Vector2 velocity = (transform.position - player.transform.position).normalized * Speed;
+                rb.velocity = velocity;
+                coroutine = StartCoroutine(StrafeTimer());
+            }
+
+
+        }
+
+        IEnumerator StrafeTimer() // Coroutine to wait x seconds to switch to chase state from strafing.
+        {
+            yield return new WaitForSeconds(2f);
+            _currentState = State.Chase;
+            coroutine = null;
+        }
+
+
+
+
+    }
+
+
+}
---
+++ Assets/Sixten Assets/BossScript.cs
@@ -0,0 +1,96 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossScript : MonoBehaviour
+{
+    GameObject DamageMod;
+    public Weapons Damage;
+
+    GameObject phealthmod;
+    public PlayerHelth pHealth;
+    public bool eInReach;
+    [SerializeField] GameObject thingToSpawn;
+    public bool canDoDamage;
+    private bool candie = true;
+    private Transform EnemyPosition;
+    float timer;
+    public bool pInReach;
+
+
+
+    [SerializeField] public int eHealth = 200;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        DamageMod = GameObject.Find("player");
+        Damage = DamageMod.GetComponent<Weapons>();
+        phealthmod = GameObject.Find("Health Maneger");
+        pHealth = phealthmod.GetComponent<PlayerHelth>();
+        EnemyPosition = GetComponent<Transform>();
+
+    }
+
+    private void Die()
+    {
+
+        canDoDamage = false;
+        Destroy(gameObject);
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("PlayerAttackbox"))
+        {
+            pInReach = false;
+        }
+        if (collision.CompareTag("PlayerHurtbox"))
+        {
+            eInReach = false;
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("PlayerAttackbox"))
+        {
+            pInReach = true;
+        }
+
+        if (collision.CompareTag("PlayerHurtbox"))
+        {
+            eInReach = true;
+        }
+
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.N) && pInReach == true)
+        {
+            eHealth -= Damage.Damage;
+        }
+        if (eHealth <= 0)
+        {
+            Instantiate(thingToSpawn, EnemyPosition);
+            Destroy(gameObject);
+        }
+
+
+
+        if (timer >= 4 && eInReach == true)
+        {
+            pHealth.Health -= 20;
+            timer = 0;
+        }
+        timer += 1 * Time.deltaTime;
+
+        if (eHealth <= 0 && candie)
+        {
+            candie = false;
+            Instantiate(thingToSpawn, EnemyPosition.position, Quaternion.identity);
+            Invoke("Die", 1);
+        }
+    }
+}
