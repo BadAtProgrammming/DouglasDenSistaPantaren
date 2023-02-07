9be1e69968435b8ae2eb6e2f5b97f63c6058eda0
BadAtProgrammming <benjamin.nord@elev.ga.lbs.se>
2023-02-07 09:31:08 +0100

    Update Weapons.cs

--- Assets/Neo Assets/Weapons.cs
+++ Assets/Neo Assets/Weapons.cs
@@ -40,7 +40,7 @@ public class Weapons : MonoBehaviour
 
     public void Dura()
     {
-        print("hej");
+        print("Durability -1");
         Durability -= 1;
         if (Durability <= 0)
         {
