20c61269da254fb999eafc1149ebb23984a3c428
Dev_ <patrykos1010@gmail.com>
2023-02-07 10:55:44 +0100

    Escape question update
    a

--- Assets/Dev(Patryk) Assets/escapequestion.cs
+++ Assets/Dev(Patryk) Assets/escapequestion.cs
@@ -12,7 +12,7 @@ public class escapequestion : MonoBehaviour
     RectTransform transformcanvas;
 
     // Start is called before the first frame update
-    void Start()
+    void Start() //Reference the referencey references (Im dead inside now) -Dev
     {
         canvasobject = GameObject.Find("EscapeQuestion");
         canvasdefined = canvasobject.GetComponent<Canvas>();
@@ -20,7 +20,7 @@ public class escapequestion : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+    void Update() //By now, you shoul know what update is, and so do I... Yeah bscly yeah checks if escape duuh! -Dev
     {
         if (Input.GetKeyDown(KeyCode.Escape))
         {
@@ -29,7 +29,7 @@ public class escapequestion : MonoBehaviour
     }
 
 
-    public void runitback()
+    public void runitback() //RUN IT BACK PRIVATE! (Resets the canvas position, ik im a memer dont worry) -Dev
     {
         transformcanvas.anchoredPosition = new Vector3(0f,1000f,0f);
     }
